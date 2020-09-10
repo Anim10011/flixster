ede6c07d54cbd5d9a0f744586ae5c7a336e36935
Min-yuan Lee <mina849669@gmail.com>
2020-09-09 17:16:13 -0700

    complete minimum assignment requirements

---
+++ flixster/MovieGirdCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGirdCell.swift
+//  flixster
+//
+//  Created by Mina Lee on 9/9/20.
+//  Copyright © 2020 mina849669@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGirdCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
